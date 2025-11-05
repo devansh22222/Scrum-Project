190d3d3d621f542b3a0148dee4ad92a636a413ab
Devansh Oad <devanshoad123@gmail.com>
2025-11-05 12:08:56 +0530

    Added login API successfully

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -42,4 +42,40 @@ const register = async (req,res)=>{
     }
 }
 
-module.exports = {register}
+const login = async (req,res)=>{
+    const {email, password} = req.body
+
+    if(!email || !password){
+        return res.json({success: false, message: "Please fill all details"})
+    }
+
+    try {
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.json({success: false, message: "User Doesn't exists"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            return res.json({success: false, message: "Wrong password"})
+        }
+
+         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:"7d"})
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure : process.env.NODE_ENV  === "production",
+            sameSite:  process.env.NODE_ENV  === "production" ? "none" : "strict",
+            maxAge: 7*24*60*60*1000
+        })
+        return res.json({success: true, message: "User logged In succesfully"})
+
+    } catch (error) {
+        console.log(error)
+        return res.json({success: false, message: error.message})
+    }
+}
+
+module.exports = {register, login}
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const { register } = require("../controllers/authController")
+const { register, login } = require("../controllers/authController")
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register)
+authRouter.post("/login", login)
 
 module.exports = authRouter;
